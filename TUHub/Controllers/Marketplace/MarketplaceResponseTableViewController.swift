f613abd14b62244a471a7489292e8d04f1370ad0
Brijesh Nayak <tue68553@temple.edu>
2017-04-02 14:27:48 -0400

    Added missing table view section title in "MarketplaceResponseTableViewController"

--- TUHub/Controllers/Marketplace/MarketplaceResponseTableViewController.swift
+++ TUHub/Controllers/Marketplace/MarketplaceResponseTableViewController.swift
@@ -21,11 +21,6 @@ class MarketplaceResponseTableViewController: UITableViewController {
         tableView.rowHeight = UITableViewAutomaticDimension
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -34,6 +29,19 @@ class MarketplaceResponseTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+            
+        case 0:
+            return "Contact Information"
+        case 1:
+            return "Response"
+
+        default:
+            return nil
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
