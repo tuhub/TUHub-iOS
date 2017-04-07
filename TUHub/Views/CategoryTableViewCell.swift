284de21ad25142e006ec0c1d900d11e45316ede5
Brijesh Nayak <tue68553@temple.edu>
2017-04-06 23:26:23 -0400

    Improved UI for AddListing, and added camera functionality.

---
+++ TUHub/Views/CategoryTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryTableViewCell.swift
+//  TUHub
+//
+//  Created by Brijesh Nayak on 4/6/17.
+//  Copyright © 2017 Temple University. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryButton: UIButton!
+    
+}
---
+++ TUHub/Views/ImageTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageTableViewCell.swift
+//  TUHub
+//
+//  Created by Brijesh Nayak on 4/6/17.
+//  Copyright © 2017 Temple University. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var takePicture: UIButton!
+    
+    @IBOutlet weak var importPicture: UIButton!
+}
