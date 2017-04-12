fa6f0d82e3703949c6e6ebd243db0f1c9d879cd1
Connor Crawford <cdcraw@icloud.com>
2017-04-12 11:31:00 -0400

    Commit before merge

---
+++ TUHub/Utilities/YLPClient+Secret.swift
@@ -0,0 +1,14 @@
+//
+//  YLPClient+Secret.swift
+//  TUHub
+//
+//  Created by Connor Crawford on 4/11/17.
+//  Copyright © 2017 Temple University. All rights reserved.
+//
+
+import YelpAPI
+
+extension YLPClient {
+    static let id = "24fpHfJygvv9D-G0DjVtDg"
+    static let secret = "aAXSjVX9kIEr9VKewSCpyo3skHGaqTcnQw9dy9KJBqKkPtLHRzMXOE5x3KPWJHu6"
+}
